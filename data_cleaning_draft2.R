d7a6da1f2458f4846c3a0372c204268ceffc382b
Karim Awad <karim.awad17@imperial.ac.uk>
2017-10-06 15:30:33 +0100

    updates

--- data_cleaning_draft2.R
+++ data_cleaning_draft2.R
@@ -9,6 +9,13 @@ library(stringr)
 library(tidyr)
 
 old_data <- read.csv("./Drafts/movie_metadata.csv", stringsAsFactors = FALSE)
+head(old_data)
+
+film_country <- old_data %>%
+  group_by(country) %>%
+  summarise(n=n())
+print(film_country)
+
 filter_data <- filter(old_data, country=="USA", language=="English")
 select_data <- select(filter_data, movie_title, title_year, director_name, duration, gross, genres, num_voted_users, num_user_for_reviews, budget, imdb_score, movie_facebook_likes, actor_1_name, actor_2_name, actor_3_name)
 select_data_filter <- filter(select_data, title_year >= 2011, gross !=0, gross != "NA", gross != "", imdb_score != 0, imdb_score != "NA", imdb_score != "", budget != 0, budget != "NA", budget != "", movie_facebook_likes != 0, movie_facebook_likes != "NA", movie_facebook_likes != "")
--- yscreening.R
+++ yscreening.R
@@ -57,11 +57,15 @@ cor.test(returns_higher1, returns_imdb1)
 ##Scatterplot data without outlier
 
 ggplot(data=newdata11_15) + geom_jitter(aes(x=return <1, y=movie_facebook_likes)) + xlab("How do facebook likes compare for films that have made <1.0x money?")
-returns_higher1 <- newdata11_15[newdata11_15[,"return"] >1, "return"]
-returns_fb1 <- newdata11_15[newdata11_15[,"return"] >1, "movie_facebook_likes"]
-returns_imdb1 <- newdata11_15[newdata11_15[,"return"] >1, "imdb_score"]
-return_data <- data.frame(returns_higher1, returns_fb1, returns_imdb1)
+returns_higher2 <- newdata11_15[newdata11_15[,"return"] >1, "return"]
+returns_fb2 <- newdata11_15[newdata11_15[,"return"] >1, "movie_facebook_likes"]
+returns_imdb2 <- newdata11_15[newdata11_15[,"return"] >1, "imdb_score"]
+return_data2 <- data.frame(returns_higher2, returns_fb2, returns_imdb2)
 
-ggplot(data=return_data) + geom_jitter(aes(x=returns_higher1, y=returns_imdb1)) + xlab("How do facebook likes compare for films that have made <1.0x money?") +xlim(1,5) + geom_smooth(aes(x=returns_higher1, y=returns_imdb1))
+head(return_data2)
 
-cor.test(returns_higher1, returns_imdb1)
+ggplot(data=return_data2) + geom_jitter(aes(x=returns_higher2, y=returns_imdb2)) + 
+  xlab("How do facebook likes compare for films that have made <1.0x money?") +
+  xlim(1,5) + geom_smooth(aes(x=returns_higher2, y=returns_imdb2)) +ylim(5,10)
+
+cor.test(returns_higher2, returns_imdb2)
