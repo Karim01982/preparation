9f127c0f2140faaec3eb2bdb82ac62f8d1cb7492
nirbhay-sharma <nirbhay.sharma17@imperial.ac.uk>
2017-10-06 17:30:09 +0100

    final model for gross...61% fit

--- regression_ns.R
+++ regression_ns.R
@@ -1,1 +1,15 @@
 #regressions
+model1 <- lm(data=data11_15, gross ~ (num_voted_users))
+summary(model1)
+
+model2 <- lm(data=data11_15, gross ~ (num_voted_users + budget))
+summary(model2)
+
+model3 <- lm(data=data11_15, gross ~ (num_voted_users + budget + actiondummy + scifidummy + thrillerdummy))
+summary(model3)
+
+model4 <- lm(data=data11_15, gross ~ (num_voted_users + budget + actiondummy + title_year + actor_oscars))
+summary(model4)
+
+library(car)
+residualPlot(model4)
