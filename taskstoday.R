80e3babc5de1716f405586b6fca34c50fcb690c5
Karim Awad <karim.awad17@imperial.ac.uk>
2017-10-08 12:00:09 +0100

    initial thoughts

---
+++ taskstoday.R
@@ -0,0 +1,6 @@
+1) Coming up with a hypothesis 
+2) Back-testing with 2016 data
+3) Looking to skewness and implications on our findings
+4) Incorporating residual plots
+5) Functionality for tables and results (e.g. stargazer; summary of descriptive statistics, etc)
+6) Data-cleaning (e.g. changing from facebook likes to number of users; being clear on any NAs / blank cells)
