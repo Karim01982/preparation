60a1042dca1f696d3025c0e9322e919f0f58d175
Karim Awad <karim.awad17@imperial.ac.uk>
2017-10-08 12:13:40 +0100

    amendment

--- taskstoday.R
+++ taskstoday.R
@@ -4,3 +4,8 @@
 4) Incorporating residual plots
 5) Functionality for tables and results (e.g. stargazer; summary of descriptive statistics, etc)
 6) Data-cleaning (e.g. changing from facebook likes to number of users; being clear on any NAs / blank cells)
+7) Conditional descriptive statistics
+
+Hypothesis testing
+
+US vs Non-US
